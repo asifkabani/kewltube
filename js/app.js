110e1338d80b95c85c2e8a72b1b2cbefa7e99a9e
Asif Kabani <asifkabani@users.noreply.github.com>
2017-07-04 00:42:42 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 /*
 KewlTube - YouTube API Implementation
+
+Low Level:
+- Variable to hold the API key.
+- Variable to hold the input of the search field.
+- Variable to put the result of the user input, input into the API call string.
+- Variable to hold the location of where the results will display on the page.
+- Variable to hold the YouTube search URL to make the API call.
+- Variable to attach the API key to the result call.
+- Variable to hold the rendered HTML template for the search results.
+- Variable to get the data from the API, with two parameters, searchTerm and callback.
+- Variable to render the HTML from the API into the page.
+- Variable for the submit button.
+- Variable to get the thumbnail image for a video search from the API.
+- Variable to hold the value of the page where the thumbnail is located (video page).
+- Variable to get the channel from each video result.
+- Variable to hold value of show more results (previous and next from the JSON).
+- Variable to hold the value for the image url to show on to the page.
+
+- Push the API key last on search
 */
+
+
 
 // Create a function to get the data. Pass two parameters, one for the searchTerm
 // and one for the callback.
@@ -25,9 +46,10 @@ function successCall(data) {
     // of the video.
     var itemThumbnailMediumUrl = item.snippet.thumbnails.medium.url;
     var itemPageId = item.id.videoId;
+    var itemTitle = item.snippet.title;
     var createPageUrl = 'https://www.youtube.com/watch?v=' + itemPageId;
     return $('#photos').append(
-      '<a href="' + createPageUrl + '" target="_blank">' + '<img src="' +  itemThumbnailMediumUrl + '"/></a>'
+      '<a href="' + createPageUrl + '" target="_blank">' + '<div class="overlay">' + '<p>' + itemTitle + '</p>' + '<img src="' +  itemThumbnailMediumUrl + '"/></div></a>'
     );
   });
 }
@@ -39,4 +61,33 @@ $('.js-form').submit(function(event) {
   searchTerm = $('.js-input').val();
   // Run the getData function with the search term and the successCall.
   getData(searchTerm, successCall)
+  $("#photos").empty().append("");
 });
+
+// var kewlTube = function () {
+//   var apiObj = {
+//     key: 'AIzaSyBMm-e7xij-SURbVFOzlT8sKPWPhxxoUSU',
+//     q: SEARCHTERM,
+//     url: 'https://www.googleapis.com/youtube/v3',
+//     ,
+//
+//   var getData = function(searchTerm, callback) {
+//     var query = {
+//       q: searchTerm + " in:name"
+//     }
+//
+// var apiObj = {
+//   q:SEARCHTERM,
+//   key:API_KEY
+// }
+//
+//   subtract = function(a, b) {
+//     return a - b;
+//   };
+//
+//   return {
+//     add: add,
+//     searchUrl: searchUrl,
+//     subtract: subtract
+//   };
+// }();
